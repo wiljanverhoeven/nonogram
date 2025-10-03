2bd1ef7794b25a62d7dbde702cf542bf6fe6ec3e
wiljanverhoeven <wiljanverhoeven@xs4all.nl>
2025-10-03 14:48:09 +0200

    puzzle

--- nonogram/Form1.Designer.cs
+++ nonogram/Form1.Designer.cs
@@ -51,6 +51,22 @@
             button3 = new Button();
             button2 = new Button();
             button1 = new Button();
+            label3 = new Label();
+            label4 = new Label();
+            label5 = new Label();
+            label6 = new Label();
+            label7 = new Label();
+            label8 = new Label();
+            label9 = new Label();
+            label10 = new Label();
+            label11 = new Label();
+            label12 = new Label();
+            label13 = new Label();
+            label14 = new Label();
+            label15 = new Label();
+            label16 = new Label();
+            label17 = new Label();
+            label18 = new Label();
             panel13.SuspendLayout();
             SuspendLayout();
             // 
@@ -249,11 +265,176 @@
             button1.UseVisualStyleBackColor = true;
             button1.Click += button1_Click;
             // 
+            // label3
+            // 
+            label3.AutoSize = true;
+            label3.Location = new Point(487, 28);
+            label3.Name = "label3";
+            label3.Size = new Size(13, 15);
+            label3.TabIndex = 14;
+            label3.Text = "1";
+            // 
+            // label4
+            // 
+            label4.AutoSize = true;
+            label4.Location = new Point(487, 43);
+            label4.Name = "label4";
+            label4.Size = new Size(13, 15);
+            label4.TabIndex = 15;
+            label4.Text = "1";
+            // 
+            // label5
+            // 
+            label5.AutoSize = true;
+            label5.Location = new Point(413, 31);
+            label5.Name = "label5";
+            label5.Size = new Size(13, 15);
+            label5.TabIndex = 16;
+            label5.Text = "2";
+            // 
+            // label6
+            // 
+            label6.AutoSize = true;
+            label6.Location = new Point(330, 31);
+            label6.Name = "label6";
+            label6.Size = new Size(13, 15);
+            label6.TabIndex = 17;
+            label6.Text = "4";
+            // 
+            // label7
+            // 
+            label7.AutoSize = true;
+            label7.Location = new Point(249, 28);
+            label7.Name = "label7";
+            label7.Size = new Size(13, 15);
+            label7.TabIndex = 18;
+            label7.Text = "1";
+            label7.Click += label7_Click;
+            // 
+            // label8
+            // 
+            label8.AutoSize = true;
+            label8.Location = new Point(249, 43);
+            label8.Name = "label8";
+            label8.Size = new Size(13, 15);
+            label8.TabIndex = 19;
+            label8.Text = "1";
+            // 
+            // label9
+            // 
+            label9.AutoSize = true;
+            label9.Location = new Point(174, 28);
+            label9.Name = "label9";
+            label9.Size = new Size(13, 15);
+            label9.TabIndex = 20;
+            label9.Text = "2";
+            label9.Click += label9_Click;
+            // 
+            // label10
+            // 
+            label10.AutoSize = true;
+            label10.Location = new Point(174, 43);
+            label10.Name = "label10";
+            label10.Size = new Size(13, 15);
+            label10.TabIndex = 21;
+            label10.Text = "1";
+            label10.Click += label10_Click;
+            // 
+            // label11
+            // 
+            label11.AutoSize = true;
+            label11.Location = new Point(90, 117);
+            label11.Name = "label11";
+            label11.Size = new Size(13, 15);
+            label11.TabIndex = 22;
+            label11.Text = "1";
+            // 
+            // label12
+            // 
+            label12.AutoSize = true;
+            label12.Location = new Point(109, 117);
+            label12.Name = "label12";
+            label12.Size = new Size(13, 15);
+            label12.TabIndex = 23;
+            label12.Text = "1";
+            label12.Click += label12_Click;
+            // 
+            // label13
+            // 
+            label13.AutoSize = true;
+            label13.Location = new Point(99, 194);
+            label13.Name = "label13";
+            label13.Size = new Size(13, 15);
+            label13.TabIndex = 24;
+            label13.Text = "3";
+            // 
+            // label14
+            // 
+            label14.AutoSize = true;
+            label14.Location = new Point(90, 272);
+            label14.Name = "label14";
+            label14.Size = new Size(13, 15);
+            label14.TabIndex = 25;
+            label14.Text = "1";
+            // 
+            // label15
+            // 
+            label15.AutoSize = true;
+            label15.Location = new Point(109, 272);
+            label15.Name = "label15";
+            label15.Size = new Size(13, 15);
+            label15.TabIndex = 26;
+            label15.Text = "1";
+            // 
+            // label16
+            // 
+            label16.AutoSize = true;
+            label16.Location = new Point(99, 353);
+            label16.Name = "label16";
+            label16.Size = new Size(13, 15);
+            label16.TabIndex = 27;
+            label16.Text = "3";
+            // 
+            // label17
+            // 
+            label17.AutoSize = true;
+            label17.Location = new Point(90, 438);
+            label17.Name = "label17";
+            label17.Size = new Size(13, 15);
+            label17.TabIndex = 28;
+            label17.Text = "1";
+            label17.Click += label17_Click;
+            // 
+            // label18
+            // 
+            label18.AutoSize = true;
+            label18.Location = new Point(109, 438);
+            label18.Name = "label18";
+            label18.Size = new Size(13, 15);
+            label18.TabIndex = 29;
+            label18.Text = "2";
+            // 
             // Form1
             // 
             AutoScaleDimensions = new SizeF(7F, 15F);
             AutoScaleMode = AutoScaleMode.Font;
             ClientSize = new Size(842, 531);
+            Controls.Add(label18);
+            Controls.Add(label17);
+            Controls.Add(label16);
+            Controls.Add(label15);
+            Controls.Add(label14);
+            Controls.Add(label13);
+            Controls.Add(label12);
+            Controls.Add(label11);
+            Controls.Add(label10);
+            Controls.Add(label9);
+            Controls.Add(label8);
+            Controls.Add(label7);
+            Controls.Add(label6);
+            Controls.Add(label5);
+            Controls.Add(label4);
+            Controls.Add(label3);
             Controls.Add(panel13);
             Controls.Add(panel12);
             Controls.Add(panel11);
@@ -276,6 +457,7 @@
             panel13.PerformLayout();
             ResumeLayout(false);
             PerformLayout();
+
         }
 
         #endregion
@@ -300,6 +482,22 @@
         private Button button1;
         private Label label1;
         private Label label2;
+        private Label label3;
+        private Label label4;
+        private Label label5;
+        private Label label6;
+        private Label label7;
+        private Label label8;
+        private Label label9;
+        private Label label10;
+        private Label label11;
+        private Label label12;
+        private Label label13;
+        private Label label14;
+        private Label label15;
+        private Label label16;
+        private Label label17;
+        private Label label18;
     }
 
 }
--- nonogram/Form1.cs
+++ nonogram/Form1.cs
@@ -21,39 +21,39 @@ namespace nonogram
         private int[,] solution;
 
         private void Form1_Load(object sender, EventArgs e)
-            {
-                int rows = 5;
-                int cols = 5;
+        {
+            int rows = 5;
+            int cols = 5;
 
-                solution = new int[,]
-                {
+            solution = new int[,]
+            {
             {1,0,1,0,0},
             {1,1,1,0,0},
             {0,0,1,0,1},
             {0,1,1,1,0},
             {1,0,0,1,1}
-                };
+            };
 
-                gridButtons = new Button[rows, cols];
+            gridButtons = new Button[rows, cols];
 
-                tableLayoutPanel1.Controls.Clear();
-                tableLayoutPanel1.RowCount = rows;
-                tableLayoutPanel1.ColumnCount = cols;
+            tableLayoutPanel1.Controls.Clear();
+            tableLayoutPanel1.RowCount = rows;
+            tableLayoutPanel1.ColumnCount = cols;
 
-                for (int r = 0; r < rows; r++)
+            for (int r = 0; r < rows; r++)
+            {
+                for (int c = 0; c < cols; c++)
                 {
-                    for (int c = 0; c < cols; c++)
-                    {
-                        Button btn = new Button();
-                        btn.Dock = DockStyle.Fill;
-                        btn.Tag = (r, c); // store position
-                        btn.BackColor = Color.White;
-                        btn.MouseDown += GridButton_MouseDown;
+                    Button btn = new Button();
+                    btn.Dock = DockStyle.Fill;
+                    btn.Tag = (r, c); // store position
+                    btn.BackColor = Color.White;
+                    btn.MouseDown += GridButton_MouseDown;
 
                     gridButtons[r, c] = btn;
-                        tableLayoutPanel1.Controls.Add(btn, c, r);
-                    }
+                    tableLayoutPanel1.Controls.Add(btn, c, r);
                 }
+            }
         }
 
         private void GridButton_MouseDown(object sender, MouseEventArgs e)
@@ -62,17 +62,16 @@ namespace nonogram
 
             if (e.Button == MouseButtons.Left)
             {
-                // toggle black fill
                 btn.BackColor = btn.BackColor == Color.Black ? Color.White : Color.Black;
                 btn.Text = "";
-            }
 
-            // Check if the puzzle is solved
-            if (CheckWin())
-            {
-                MessageBox.Show("Congratulations! You solved the puzzle!");
+                if (CheckWin())
+                {
+                    MessageBox.Show("Congratulations! You solved the puzzle!");
+                }
             }
         }
+
 
         private bool CheckWin()
         {
@@ -83,16 +82,42 @@ namespace nonogram
             {
                 for (int c = 0; c < cols; c++)
                 {
-                    // 1 in solution = black, 0 = white
-                    bool isBlack = gridButtons[r, c].BackColor == Color.Black;
-                    if ((solution[r, c] == 1 && !isBlack) || (solution[r, c] == 0 && isBlack))
-                    {
+                    bool isFilled = gridButtons[r, c].BackColor == Color.Black;
+                    if (isFilled != (solution[r, c] == 1))
                         return false; // mismatch found
-                    }
                 }
             }
             return true; // all match
         }
+
+        private List<int> GetLineNumbers(int[] line)
+        {
+            List<int> numbers = new List<int>();
+            int count = 0;
+
+            foreach (int cell in line)
+            {
+                if (cell == 1)
+                {
+                    count++;
+                }
+                else
+                {
+                    if (count > 0)
+                    {
+                        numbers.Add(count);
+                        count = 0;
+                    }
+                }
+            }
+            if (count > 0) numbers.Add(count);
+
+            if (numbers.Count == 0) numbers.Add(0); // no filled cells
+            return numbers;
+        }
+
+
+
 
 
         private void button1_Click(object sender, EventArgs e)
@@ -116,6 +141,36 @@ namespace nonogram
         }
 
         private void label2_Click(object sender, EventArgs e)
+        {
+
+        }
+
+        private void label3_Click(object sender, EventArgs e)
+        {
+
+        }
+
+        private void label7_Click(object sender, EventArgs e)
+        {
+
+        }
+
+        private void label9_Click(object sender, EventArgs e)
+        {
+
+        }
+
+        private void label10_Click(object sender, EventArgs e)
+        {
+
+        }
+
+        private void label12_Click(object sender, EventArgs e)
+        {
+
+        }
+
+        private void label17_Click(object sender, EventArgs e)
         {
 
         }
