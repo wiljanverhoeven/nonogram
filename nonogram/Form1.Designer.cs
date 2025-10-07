4372849a7ad1c69729a179cb876de5bb7156f984
wiljanverhoeven <wiljanverhoeven@xs4all.nl>
2025-10-07 15:02:16 +0200

    made pictogram puzzle random

--- nonogram/Form1.Designer.cs
+++ nonogram/Form1.Designer.cs
@@ -46,6 +46,7 @@
             panel11 = new Panel();
             panel12 = new Panel();
             panel13 = new Panel();
+            button2 = new Button();
             label2 = new Label();
             label1 = new Label();
             button3 = new Button();
@@ -60,12 +61,6 @@
             label10 = new Label();
             label11 = new Label();
             label12 = new Label();
-            label13 = new Label();
-            label14 = new Label();
-            label15 = new Label();
-            label16 = new Label();
-            label17 = new Label();
-            label18 = new Label();
             panel13.SuspendLayout();
             SuspendLayout();
             // 
@@ -207,6 +202,7 @@
             // 
             panel13.BackColor = SystemColors.ActiveCaption;
             panel13.BorderStyle = BorderStyle.FixedSingle;
+            panel13.Controls.Add(button2);
             panel13.Controls.Add(label2);
             panel13.Controls.Add(label1);
             panel13.Controls.Add(button3);
@@ -216,10 +212,20 @@
             panel13.Size = new Size(158, 340);
             panel13.TabIndex = 13;
             // 
+            // button2
+            // 
+            button2.Location = new Point(32, 107);
+            button2.Name = "button2";
+            button2.Size = new Size(92, 27);
+            button2.TabIndex = 5;
+            button2.Text = "Start";
+            button2.UseVisualStyleBackColor = true;
+            button2.Click += button2_Click;
+            // 
             // label2
             // 
             label2.AutoSize = true;
-            label2.Location = new Point(50, 69);
+            label2.Location = new Point(58, 70);
             label2.Name = "label2";
             label2.Size = new Size(38, 15);
             label2.TabIndex = 4;
@@ -238,7 +244,7 @@
             // 
             // button3
             // 
-            button3.Location = new Point(32, 142);
+            button3.Location = new Point(32, 176);
             button3.Name = "button3";
             button3.Size = new Size(92, 30);
             button3.TabIndex = 2;
@@ -248,7 +254,7 @@
             // 
             // button1
             // 
-            button1.Location = new Point(32, 106);
+            button1.Location = new Point(32, 140);
             button1.Name = "button1";
             button1.Size = new Size(92, 30);
             button1.TabIndex = 0;
@@ -259,163 +265,89 @@
             // label3
             // 
             label3.AutoSize = true;
-            label3.Location = new Point(487, 28);
+            label3.Location = new Point(92, 439);
             label3.Name = "label3";
-            label3.Size = new Size(13, 15);
+            label3.Size = new Size(0, 15);
             label3.TabIndex = 14;
-            label3.Text = "1";
             // 
             // label4
             // 
             label4.AutoSize = true;
-            label4.Location = new Point(487, 43);
+            label4.Location = new Point(92, 359);
             label4.Name = "label4";
-            label4.Size = new Size(13, 15);
+            label4.Size = new Size(0, 15);
             label4.TabIndex = 15;
-            label4.Text = "1";
+            label4.Click += label4_Click;
             // 
             // label5
             // 
             label5.AutoSize = true;
-            label5.Location = new Point(413, 31);
+            label5.Location = new Point(92, 274);
             label5.Name = "label5";
-            label5.Size = new Size(13, 15);
+            label5.Size = new Size(0, 15);
             label5.TabIndex = 16;
-            label5.Text = "2";
             // 
             // label6
             // 
             label6.AutoSize = true;
-            label6.Location = new Point(330, 31);
+            label6.Location = new Point(92, 192);
             label6.Name = "label6";
-            label6.Size = new Size(13, 15);
+            label6.Size = new Size(0, 15);
             label6.TabIndex = 17;
-            label6.Text = "4";
             // 
             // label7
             // 
             label7.AutoSize = true;
-            label7.Location = new Point(249, 28);
+            label7.Location = new Point(92, 110);
             label7.Name = "label7";
-            label7.Size = new Size(13, 15);
+            label7.Size = new Size(0, 15);
             label7.TabIndex = 18;
-            label7.Text = "1";
-            label7.Click += label7_Click;
             // 
             // label8
             // 
             label8.AutoSize = true;
-            label8.Location = new Point(249, 43);
+            label8.Location = new Point(162, 42);
             label8.Name = "label8";
-            label8.Size = new Size(13, 15);
+            label8.Size = new Size(0, 15);
             label8.TabIndex = 19;
-            label8.Text = "1";
             // 
             // label9
             // 
             label9.AutoSize = true;
-            label9.Location = new Point(174, 28);
+            label9.Location = new Point(244, 42);
             label9.Name = "label9";
-            label9.Size = new Size(13, 15);
+            label9.Size = new Size(0, 15);
             label9.TabIndex = 20;
-            label9.Text = "2";
-            label9.Click += label9_Click;
             // 
             // label10
             // 
             label10.AutoSize = true;
-            label10.Location = new Point(174, 43);
+            label10.Location = new Point(322, 42);
             label10.Name = "label10";
-            label10.Size = new Size(13, 15);
+            label10.Size = new Size(0, 15);
             label10.TabIndex = 21;
-            label10.Text = "1";
-            label10.Click += label10_Click;
             // 
             // label11
             // 
             label11.AutoSize = true;
-            label11.Location = new Point(90, 117);
+            label11.Location = new Point(403, 42);
             label11.Name = "label11";
-            label11.Size = new Size(13, 15);
+            label11.Size = new Size(0, 15);
             label11.TabIndex = 22;
-            label11.Text = "1";
             // 
             // label12
             // 
             label12.AutoSize = true;
-            label12.Location = new Point(109, 117);
+            label12.Location = new Point(483, 42);
             label12.Name = "label12";
-            label12.Size = new Size(13, 15);
+            label12.Size = new Size(0, 15);
             label12.TabIndex = 23;
-            label12.Text = "1";
-            label12.Click += label12_Click;
-            // 
-            // label13
-            // 
-            label13.AutoSize = true;
-            label13.Location = new Point(99, 194);
-            label13.Name = "label13";
-            label13.Size = new Size(13, 15);
-            label13.TabIndex = 24;
-            label13.Text = "3";
-            // 
-            // label14
-            // 
-            label14.AutoSize = true;
-            label14.Location = new Point(90, 272);
-            label14.Name = "label14";
-            label14.Size = new Size(13, 15);
-            label14.TabIndex = 25;
-            label14.Text = "1";
-            // 
-            // label15
-            // 
-            label15.AutoSize = true;
-            label15.Location = new Point(109, 272);
-            label15.Name = "label15";
-            label15.Size = new Size(13, 15);
-            label15.TabIndex = 26;
-            label15.Text = "1";
-            // 
-            // label16
-            // 
-            label16.AutoSize = true;
-            label16.Location = new Point(99, 353);
-            label16.Name = "label16";
-            label16.Size = new Size(13, 15);
-            label16.TabIndex = 27;
-            label16.Text = "3";
-            // 
-            // label17
-            // 
-            label17.AutoSize = true;
-            label17.Location = new Point(90, 438);
-            label17.Name = "label17";
-            label17.Size = new Size(13, 15);
-            label17.TabIndex = 28;
-            label17.Text = "1";
-            label17.Click += label17_Click;
-            // 
-            // label18
-            // 
-            label18.AutoSize = true;
-            label18.Location = new Point(109, 438);
-            label18.Name = "label18";
-            label18.Size = new Size(13, 15);
-            label18.TabIndex = 29;
-            label18.Text = "2";
             // 
             // Form1
             // 
             AutoScaleDimensions = new SizeF(7F, 15F);
             AutoScaleMode = AutoScaleMode.Font;
             ClientSize = new Size(838, 499);
-            Controls.Add(label18);
-            Controls.Add(label17);
-            Controls.Add(label16);
-            Controls.Add(label15);
-            Controls.Add(label14);
-            Controls.Add(label13);
             Controls.Add(label12);
             Controls.Add(label11);
             Controls.Add(label10);
@@ -471,6 +403,8 @@
         private Button button3;
         private Button button1;
         private Label label1;
+        private Label label2;
+        private Button button2;
         private Label label3;
         private Label label4;
         private Label label5;
@@ -481,13 +415,6 @@
         private Label label10;
         private Label label11;
         private Label label12;
-        private Label label13;
-        private Label label14;
-        private Label label15;
-        private Label label16;
-        private Label label17;
-        private Label label18;
-        private Label label2;
     }
 
 }
--- nonogram/Form1.cs
+++ nonogram/Form1.cs
@@ -1,4 +1,4 @@
-namespace nonogram
+﻿namespace nonogram
 {
     public partial class Form1 : Form
     {
@@ -23,19 +23,22 @@ namespace nonogram
 
         private void Form1_Load(object sender, EventArgs e)
         {
+            button1.Enabled = false; // Reset
+            button3.Enabled = false; // Pause
+            label2.Text = "Time: 00:00";
+            tableLayoutPanel1.Enabled = false;
+        }
+
+        private void StartGame()
+        {
             int rows = 5;
             int cols = 5;
 
-            solution = new int[,]
-            {
-            {1,0,1,0,0},
-            {1,1,1,0,0},
-            {0,0,1,0,1},
-            {0,1,1,1,0},
-            {1,0,0,1,1}
-            };
+            solution = GenerateRandomSolution(rows, cols);
 
-            gridButtons = new Button[rows, cols];
+            gridButtons = new Button[rows, cols]; // Initialize gridButtons array
+
+            tableLayoutPanel1.SuspendLayout();
 
             tableLayoutPanel1.Controls.Clear();
             tableLayoutPanel1.RowCount = rows;
@@ -50,26 +53,184 @@ namespace nonogram
                     btn.Tag = (r, c);
                     btn.BackColor = Color.White;
                     btn.MouseDown += GridButton_MouseDown;
+                    btn.Enabled = true;
 
                     gridButtons[r, c] = btn;
                     tableLayoutPanel1.Controls.Add(btn, c, r);
                 }
             }
 
-            // Timer setup
+            tableLayoutPanel1.ResumeLayout();
+
+            // Update row clues (labels 3 to 7, from bottom to top)
+            // So label3 is bottom row, label7 is top row
+            for (int r = 0; r < rows; r++)
+            {
+                string clue = GenerateClueForLine(GetRow(solution, r));
+                // labels 3-7 bottom to top => label3 for row 4, label4 for row 3, ..., label7 for row 0
+                int labelIndex = 7 - r;
+                SetRowLabel(labelIndex, clue);
+            }
+
+            // Update column clues (labels 8 to 12, left to right)
+            for (int c = 0; c < cols; c++)
+            {
+                string clue = GenerateClueForLine(GetColumn(solution, c));
+                SetColumnLabel(8 + c, clue);
+            }
+
             elapsedSeconds = 0;
             label2.Text = "Time: 00:00";
-            timer1.Interval = 1000; // 1 second
-            timer1.Tick += Timer1_Tick;
+            timer1.Interval = 1000;
             timer1.Start();
+
+            button1.Enabled = true; // Reset
+            button3.Enabled = true; // Pause
         }
 
-        private void Timer1_Tick(object sender, EventArgs e)
+        private int[,] GenerateRandomSolution(int rows, int cols, double fillProbability = 0.3)
+            {
+                Random rand = new Random();
+                int[,] grid = new int[rows, cols];
+
+                // Step 1: Random fill based on fillProbability
+                for (int r = 0; r < rows; r++)
+                {
+                    for (int c = 0; c < cols; c++)
+                    {
+                        grid[r, c] = (rand.NextDouble() < fillProbability) ? 1 : 0;
+                    }
+                }
+
+                // Step 2: Ensure each row has at least one '1'
+                for (int r = 0; r < rows; r++)
+                {
+                    bool hasOne = false;
+                    for (int c = 0; c < cols; c++)
+                    {
+                        if (grid[r, c] == 1)
+                        {
+                            hasOne = true;
+                            break;
+                        }
+                    }
+                    if (!hasOne)
+                    {
+                        // Pick random column and set to 1
+                        int colIndex = rand.Next(cols);
+                        grid[r, colIndex] = 1;
+                    }
+                }
+
+                // Step 3: Ensure each column has at least one '1'
+                for (int c = 0; c < cols; c++)
+                {
+                    bool hasOne = false;
+                    for (int r = 0; r < rows; r++)
+                    {
+                        if (grid[r, c] == 1)
+                        {
+                            hasOne = true;
+                            break;
+                        }
+                    }
+                    if (!hasOne)
+                    {
+                        // Pick random row and set to 1
+                        int rowIndex = rand.Next(rows);
+                        grid[rowIndex, c] = 1;
+                    }
+                }
+
+                return grid;
+            }
+
+
+        // Extract a row as an int array
+        private int[] GetRow(int[,] grid, int row)
+        {
+            int cols = grid.GetLength(1);
+            int[] result = new int[cols];
+            for (int c = 0; c < cols; c++)
+            {
+                result[c] = grid[row, c];
+            }
+            return result;
+        }
+
+        // Extract a column as an int array
+        private int[] GetColumn(int[,] grid, int col)
+        {
+            int rows = grid.GetLength(0);
+            int[] result = new int[rows];
+            for (int r = 0; r < rows; r++)
+            {
+                result[r] = grid[r, col];
+            }
+            return result;
+        }
+
+        // Generate clues string for a line of 0/1 values
+        private string GenerateClueForLine(int[] line)
+        {
+            List<int> clues = new List<int>();
+            int count = 0;
+
+            foreach (var cell in line)
+            {
+                if (cell == 1)
+                {
+                    count++;
+                }
+                else
+                {
+                    if (count > 0)
+                    {
+                        clues.Add(count);
+                        count = 0;
+                    }
+                }
+            }
+            if (count > 0) clues.Add(count);
+
+            if (clues.Count == 0) return "0"; // no filled cells, show 0 or blank
+
+            return string.Join("-", clues);
+        }
+
+        // Set the row clue label based on label index (3 to 7)
+        private void SetRowLabel(int labelNumber, string text)
+        {
+            switch (labelNumber)
+            {
+                case 3: label3.Text = text; break;
+                case 4: label4.Text = text; break;
+                case 5: label5.Text = text; break;
+                case 6: label6.Text = text; break;
+                case 7: label7.Text = text; break;
+            }
+        }
+
+        // Set the column clue label based on label index (8 to 12)
+        private void SetColumnLabel(int labelNumber, string text)
+        {
+            switch (labelNumber)
+            {
+                case 8: label8.Text = text; break;
+                case 9: label9.Text = text; break;
+                case 10: label10.Text = text; break;
+                case 11: label11.Text = text; break;
+                case 12: label12.Text = text; break;
+            }
+        }
+
+        private void timer1_Tick(object sender, EventArgs e)
         {
             elapsedSeconds++;
             TimeSpan time = TimeSpan.FromSeconds(elapsedSeconds);
             label2.Text = $"Time: {time:mm\\:ss}";
         }
+
 
         private void GridButton_MouseDown(object sender, MouseEventArgs e)
         {
@@ -104,56 +265,63 @@ namespace nonogram
             }
 
             // Puzzle is solved
-            timer1.Stop(); // ?? stop the timer
+            timer1.Stop(); // stop the timer
             return true;
         }
 
         private void button1_Click(object sender, EventArgs e)
-            {
-                int rows = gridButtons.GetLength(0);
-                int cols = gridButtons.GetLength(1);
-
-                // Reset all buttons to white and clear text
-                for (int r = 0; r < rows; r++)
-                {
-                    for (int c = 0; c < cols; c++)
-                    {
-                        gridButtons[r, c].BackColor = Color.White;
-                        gridButtons[r, c].Text = "";
-                    }
-                }
-
-                // Reset and restart the timer
-                elapsedSeconds = 0;
-                label2.Text = "Time: 00:00";
-                timer1.Stop();    // Stop in case it was paused
-                timer1.Start();   // Start the timer fresh
-        }
-
-        private void label1_Click_1(object sender, EventArgs e)
         {
+            int rows = gridButtons.GetLength(0);
+            int cols = gridButtons.GetLength(1);
 
+            for (int r = 0; r < rows; r++)
+            {
+                for (int c = 0; c < cols; c++)
+                {
+                    gridButtons[r, c].BackColor = Color.White;
+                    gridButtons[r, c].Text = "";
+                    gridButtons[r, c].Enabled = false; // disable until start
+                }
+            }
+
+            elapsedSeconds = 0;
+            label2.Text = "Time: 00:00";
+            timer1.Stop();
+
+            tableLayoutPanel1.Enabled = false;
+            button2.Enabled = true;  // re-enable start button
+            button3.Text = "Pause";  // reset pause button text
+            button1.Enabled = false;
+            button3.Enabled = false;
         }
+
 
         private void button2_Click(object sender, EventArgs e)
         {
-
+            StartGame();
+            tableLayoutPanel1.Enabled = true; // now enable the puzzle
+            button2.Enabled = false; // prevent multiple starts
         }
 
         private void button3_Click(object sender, EventArgs e)
         {
             if (timer1.Enabled)
             {
-                // Timer is running ? pause it
+                // Timer is running → pause it
                 timer1.Stop();
                 button3.Text = "Resume";
             }
             else
             {
-                // Timer is stopped ? resume it
+                // Timer is stopped → resume it
                 timer1.Start();
                 button3.Text = "Pause";
             }
+        }
+
+        private void label1_Click_1(object sender, EventArgs e)
+        {
+
         }
 
         private void label2_Click(object sender, EventArgs e)
@@ -191,12 +359,19 @@ namespace nonogram
 
         }
 
-        private void timer1_Tick(object sender, EventArgs e)
+
+
+        private void flowLayoutPanel1_Paint(object sender, PaintEventArgs e)
         {
 
         }
 
-        private void flowLayoutPanel1_Paint(object sender, PaintEventArgs e)
+        private void label3_Click_1(object sender, EventArgs e)
+        {
+
+        }
+
+        private void label4_Click(object sender, EventArgs e)
         {
 
         }
